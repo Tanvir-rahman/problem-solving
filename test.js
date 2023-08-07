2f100b5eff9f2d070ab3c009f446ef256300cfd6
Tanvir Rahman <rahmantanvir25558@gmail.com>
2023-08-07 23:46:35 +0600

    maxProfit solved

--- test.js
+++ test.js
@@ -1,5 +1,18 @@
-function sum_of_natural_numbers(n) {
-  return (n * (n + 1)) / 2;
+function maxProfit(prices) {
+  if (!prices || prices.length === 0) {
+    return 0;
+  }
+
+  let minPrice = prices[0];
+  let maxProfit = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    minPrice = Math.min(minPrice, prices[i]);
+    maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+  }
+
+  return Math.max(maxProfit, 0);
 }
 
-console.log(sum_of_natural_numbers(2));
+prices = [7, 1, 5, 3, 6, 4]
+console.log(maxProfit(prices))
--- test.py
+++ test.py
@@ -1,22 +1,6 @@
-def perfectPeak(A):
-    n = len(A)
-    max_left = [0] * n
-    min_right = [0] * n
+def solve(A, B):
+    max_value = max(A)
+    for i in range(B):
+        # check if each element can be made equal to max_value by adding, substraction or do nothing with B
 
-    max = A[0]
-    min = A[n - 1]
-    for i in range(1, n + 1):
-        if A[i - 1] > max:
-            max = A[i - 1]
-        max_left[i - 1] = max
-        if A[n - i] < min:
-            min = A[n - i]
-        min_right[n - i - 1] = min
-
-    for i in range(1, n):
-        if max_left[i - 1] < A[i] < min_right[i]:
-            return 1
-    return 0
-
-
-print(perfectPeak([2, 3, 2]))
+solve([2, 3, 2], 1)
